6938acd8799646ea8555b791281ae0e588eb306e
Md Mustakin Mahi <63896978+Mustakin-140@users.noreply.github.com>
2021-11-08 00:06:55 +0600

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,21 @@
+#casting means specifying type of variable
+x = int(1) 
+y = int(2.8) 
+z = int(3.6566)
+
+print(x)
+print(y)
+print(z)
+
+a = float(1)
+b = float(2.5646)
+c = float(3.75)
+
+print(a)
+print(b)
+print(c)
+
+m = str("s1")
+n = str(2)
+print(m)
+print(n)
